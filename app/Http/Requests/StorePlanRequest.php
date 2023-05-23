f5e6478dc326a0f24fd5e791302ec88ec3c47835
YUN.CHHILIN <YUN.CHHILIN@PNC.LAN>
2023-05-23 20:13:20 +0700

    create plan table

---
+++ app/Http/Requests/StorePlanRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator as ValidationValidator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+class StorePlanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    protected function failedValidation(ValidationValidator $validator)
+    {
+        throw new HttpResponseException(response()->json(['success' => false, 'message' => $validator->errors()], 412));
+    }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                Rule::unique('plans')-> ignore($this->id)
+            ],
+
+            'area' => 'required',
+            'datetime' => 'required',
+            'duration' => 'required',
+            'status' => 'required',
+            'create_by_id' => 'required',
+            
+        ];
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -60,4 +60,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Drone::class);
     }
+
+    public function plans(): HasMany
+    {
+        return $this->hasMany(Plan::class);
+    }
 }
